c63bfb1045529d27bfc9e1805830ab375e200f44
Lou Ghys <loughys@gmail.com>
2021-06-26 12:24:39 +0200

    feat(pro-listing-page): improve the pro listing page display

--- __generated__/globalTypes.ts
+++ __generated__/globalTypes.ts
@@ -7,6 +7,20 @@
 // START Enums and Input Objects
 //==============================================================
 
+export enum PaymentMethodsEnum {
+  bancontact = "bancontact",
+  bankTransfer = "bankTransfer",
+  cash = "cash",
+  credit = "credit",
+  qrCode = "qrCode",
+}
+
+export interface GetArgs {
+  limit: number;
+  skip: number;
+  sort?: string | null;
+}
+
 //==============================================================
 // END Enums and Input Objects
 //==============================================================
